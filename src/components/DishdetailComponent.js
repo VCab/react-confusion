10f0dbe9ab9e820b8568109cbfb7224887bf7ccb
VCab <47830184+VCab@users.noreply.github.com>
2022-08-20 00:23:21 +0000

    functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,31 +1,63 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, List } from 'reactstrap';
 
-class DishDetail extends Component {
+function RenderDish({ dish }) {
+    return (
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
 
-    render() {
-
-        const dish = this.props.dishSelected;
-        if (dish) {
-            return (
-                <div className='container'>
-                    <div className='row'>
-                        <Card>
-                            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div></div>
-            )
-        }
+function RenderComments({comments}) {
+    if (comments.length) {
+        const commentsList = comments.map((comment) =>
+            <div>
+                <li className="mb-2">{comment.comment}</li>
+                <li className="mb-2">-- {comment.author}, {new Intl.DateTimeFormat('en-us', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</li>
+            </div>
+        );
+        return (
+            <div>
+                <h4>Comments</h4>
+                <List type="unstyled">
+                    {commentsList}
+                </List>
+            </div>
+        )
+    } else {
+        return (
+            <div></div>
+        )
     }
 }
+
+const DishDetail = (props) => {
+    const dish = props.dishSelected;
+    if (dish) {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={dish}></RenderDish>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={dish.comments}></RenderComments>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div></div>
+        )
+    }
+}
+
+
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Navbar, NavbarBrand, List } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from '../shared/dishes';
@@ -19,41 +19,6 @@ class Main extends Component {
         this.setState({ selectedDish: dishId });
     }
 
-    // renderDish(dish) {
-    //     if (dish) {
-    //         return (
-    //             <DishDetail dishSelected={this.state.selectedDish} />
-    //         )
-    //     } else {
-    //         return (
-    //             <div></div>
-    //         )
-    //     }
-    // }
-
-    renderComments(comments) {
-        if (comments.length) {
-            const commentsList = comments.map((comment) =>
-                <div>
-                    <li className="mb-2">{comment.comment}</li>
-                    <li className="mb-2">-- {comment.author}, {new Intl.DateTimeFormat('en-us', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</li>
-                </div>
-            );
-            return (
-                <div>
-                    <h4>Comments</h4>
-                    <List type="unstyled">
-                        {commentsList}
-                    </List>
-                </div>
-            )
-        } else {
-            return (
-                <div></div>
-            )
-        }
-    }
-
     render() {
         return (
             <div>
@@ -65,12 +30,7 @@ class Main extends Component {
                 <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
                 <div className='container'>
                     <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <DishDetail dishSelected={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            {this.state.selectedDish && this.renderComments(this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0].comments)}
-                        </div>
+                        <DishDetail dishSelected={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
                     </div>
                 </div>
             </div>
